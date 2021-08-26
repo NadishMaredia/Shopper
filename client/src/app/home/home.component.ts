2a9eef10a3f570812341bfc2b5abe47d72c25c4d
Nadish Maredia <nd843857@dal.ca>
2021-08-26 14:35:12 -0300

    update product page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private productService: ProductService) { 
+    this.getProducts();
+  }
 
   ngOnInit(): void {
+  }
+
+  getProducts() {
+    this.productService.getProduct()
+      .subscribe(response => {
+        console.log(response);
+      })
   }
 
 }
