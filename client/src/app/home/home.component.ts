74b4ba71a856c4739e8887dea57b31babc6a5f9e
Nadish Maredia <nd843857@dal.ca>
2021-08-27 13:04:18 -0300

    home page

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../product.service';
 
 @Component({
@@ -8,12 +9,30 @@ import { ProductService } from '../product.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { 
-  }
+  categories = ['bread','diary','fruits','seasonings','vegetables'];
+  products: any[] = [];
+  filteredProducts: any[];
+  category: string;
 
+  constructor(private productService: ProductService, 
+    private route: ActivatedRoute) {
+    route.queryParamMap.subscribe(params => {
+      this.category = params.get('category');
+      this.filteredProducts = (this.category) ?
+        this.products.filter(p => p.category === this.category) :
+        this.products;
+    });
+  }
   ngOnInit(): void {
+    this.getProducts(); 
   }
 
-  
+  getProducts() {
+    this.productService.getProduct()
+      .subscribe(response => {
+        this.products = response;
+        this.filteredProducts = this.products;
+      })
+  }
 
 }
